296b45d0f1a533ac53686e9346204cca71ad1ca0
QuyXuan <quymtx@bkhightech.vn>
2023-10-12 17:46:47 +0700

    feat: Clone WhatsApp lesson 7

--- lib/feature/welcome/pages/verification_page.dart
+++ lib/feature/welcome/pages/verification_page.dart
@@ -1,38 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatapp_messenger/common/extensions/custom_theme_extension.dart';
 import 'package:whatapp_messenger/common/widgets/custom_icon_button.dart';
+import 'package:whatapp_messenger/feature/auth/controller/auth_controller.dart';
 import 'package:whatapp_messenger/feature/auth/widgets/custom_text_field.dart';
 
-class VerificationPage extends StatefulWidget {
-  final String verificationId;
+class VerificationPage extends ConsumerWidget {
+  final String smsCodeId;
   final String phoneNumber;
+
   const VerificationPage({
     super.key,
-    required this.verificationId,
+    required this.smsCodeId,
     required this.phoneNumber,
   });
 
-  @override
-  State<VerificationPage> createState() => _VerificationPageState();
-}
-
-class _VerificationPageState extends State<VerificationPage> {
-  late TextEditingController codeController;
-
-  @override
-  void initState() {
-    codeController = TextEditingController();
-    super.initState();
+  void verifySmsCode(
+    BuildContext context,
+    WidgetRef ref,
+    String smsCode,
+  ) {
+    ref.read(authControllerProvider).verifySmsCode(
+          context: context,
+          smsCodeId: smsCodeId,
+          smsCode: smsCode,
+          mounted: true,
+        );
   }
 
   @override
-  void dispose() {
-    codeController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
@@ -81,12 +78,15 @@ class _VerificationPageState extends State<VerificationPage> {
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 80),
               child: CustomTextField(
-                controller: codeController,
                 hintText: '- - -  - - -',
                 fontSize: 30,
                 autoFocus: true,
                 keyboardType: TextInputType.number,
-                onChanged: (value) {},
+                onChanged: (value) {
+                  if (value.length == 6) {
+                    return verifySmsCode(context, ref, value);
+                  }
+                },
               ),
             ),
             const SizedBox(height: 20),
