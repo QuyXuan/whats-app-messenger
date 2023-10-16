4d4ead4b2cd06e90f22ce7b1fbce92b898f53489
QuyXuan <quymtx@bkhightech.vn>
2023-10-16 17:33:33 +0700

    feat: Clone WhatsApp lesson 12

--- lib/feature/home/pages/home_page.dart
+++ lib/feature/home/pages/home_page.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:whatapp_messenger/common/widgets/custom_icon_button.dart';
+import 'package:whatapp_messenger/feature/home/pages/call_home_page.dart';
+import 'package:whatapp_messenger/feature/home/pages/chat_home_page.dart';
+import 'package:whatapp_messenger/feature/home/pages/status_home_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(letterSpacing: 1),
+          ),
+          elevation: 1,
+          actions: [
+            CustomIconButton(onTap: () {}, icon: Icons.search),
+            CustomIconButton(onTap: () {}, icon: Icons.more_vert),
+          ],
+          bottom: const TabBar(
+            indicatorWeight: 3,
+            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+            splashFactory: NoSplash.splashFactory,
+            tabs: [
+              Tab(text: 'CHATS'),
+              Tab(text: 'STATUS'),
+              Tab(text: 'CALLS'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            ChatHomePage(),
+            StatusHomePage(),
+            CallHomePage(),
+          ],
+        ),
+      ),
     );
   }
 }
